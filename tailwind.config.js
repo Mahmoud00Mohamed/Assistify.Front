39fb38031de1d01316592b3a76cbee7fd87b4c4e
Mahmoud <mahmoud@gmail.com>
2025-03-21 23:21:43 +0200

    Finish.7

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html", // كل ملفات HTML في المشروع
+    "./**/*.js", // كل ملفات JS في المشروع
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
